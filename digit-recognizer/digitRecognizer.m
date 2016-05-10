bb66349cb5573278ccbb096f3c0c1da015b5471b
Runhe Tian <tianrunhe@gmail.com>
2016-05-09 22:01:40 -0700

    Refactor: move training of neural network to a function itself

--- digit-recognizer/digitRecognizer.m
+++ digit-recognizer/digitRecognizer.m
@@ -1,4 +1,4 @@
-function digitRecognizer(trainingFileName, predictionFileName, maxIter, lambda)
+function digitRecognizer(trainingFileName, predictionFileName)
 
 	%% Setup the parameters you will use for this exercise
 	input_layer_size  = 784;  % 28x28 Input Images of Digits
@@ -24,32 +24,11 @@ function digitRecognizer(trainingFileName, predictionFileName, maxIter, lambda)
 	m = size(X, 1);
 
 
-	%% ================ Initializing Pameters ================
-
-	fprintf('\nInitializing Neural Network Parameters ...\n')
-
-	initial_Theta1 = randInitializeWeights(input_layer_size, hidden_layer_size);
-	initial_Theta2 = randInitializeWeights(hidden_layer_size, num_labels);
-
-	% Unroll parameters
-	initial_nn_params = [initial_Theta1(:) ; initial_Theta2(:)];
-
-
 	%% =================== Training NN ===================
 
 	fprintf('\nTraining Neural Network... \n')
 
-	options = optimset('MaxIter', maxIter);
-
-	% Create "short hand" for the cost function to be minimized
-	costFunction = @(p) nnCostFunction(p, ...
-	                                   input_layer_size, ...
-	                                   hidden_layer_size, ...
-	                                   num_labels, X, y, lambda);
-
-	% Now, costFunction is a function that takes in only one argument (the
-	% neural network parameters)
-	[nn_params, cost] = fmincg(costFunction, initial_nn_params, options);
+	nn_params = trainNN(X, y, input_layer_size, hidden_layer_size, num_labels, 1.5);
 
 	% Obtain Theta1 and Theta2 back from nn_params
 	Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
---
+++ digit-recognizer/trainNN.m
@@ -0,0 +1,25 @@
+function [nn_params] = trainNN(X, y, input_layer_size, hidden_layer_size, num_labels, lambda)
+
+	%% ================ Initializing Pameters ================
+
+	fprintf('\nInitializing Neural Network Parameters ...\n')
+
+	initial_Theta1 = randInitializeWeights(input_layer_size, hidden_layer_size);
+	initial_Theta2 = randInitializeWeights(hidden_layer_size, num_labels);
+
+	% Unroll parameters
+	initial_nn_params = [initial_Theta1(:) ; initial_Theta2(:)];
+
+	options = optimset('MaxIter', 50);
+
+	% Create "short hand" for the cost function to be minimized
+	costFunction = @(p) nnCostFunction(p, ...
+	                                   input_layer_size, ...
+	                                   hidden_layer_size, ...
+	                                   num_labels, X, y, lambda);
+
+	% Now, costFunction is a function that takes in only one argument (the
+	% neural network parameters)
+	[nn_params, cost] = fmincg(costFunction, initial_nn_params, options);
+
+end
